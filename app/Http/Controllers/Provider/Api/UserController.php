8ba71ccfeca21d7ec8a73fe1f98e7695307ad9a3
axay <axayhg@gmail.com>
2018-08-28 01:24:20 +0530

    provider update

--- app/Http/Controllers/Provider/Api/UserController.php
+++ app/Http/Controllers/Provider/Api/UserController.php
@@ -10,6 +10,6 @@ class UserController extends Controller
     public function index(Request $request)
     {
 
-        return auth()->guard('provider')->user();
+        return auth()->user();
     }
 }
