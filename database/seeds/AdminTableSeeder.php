bc818f8d8dd50fdabb77211e4c866764dbcea94f
axay <axayhg@gmail.com>
2018-08-27 12:55:20 +0530

    admin name changes

--- database/seeds/AdminTableSeeder.php
+++ database/seeds/AdminTableSeeder.php
@@ -3,22 +3,24 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class AdminTableSeeder extends Seeder {
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run() {
-		DB::table( 'admins' )->truncate();
-		DB::table( 'admins' )->insert( [
-			'name'     => 'TenStudio',
-			'email'    => 'admin@tenstudio.in',
-			'password' => bcrypt( '123456' ),
-		], [
-			'name'     => 'TenStudio Demo',
-			'email'    => 'demo@tenstudio.in',
-			'password' => bcrypt( '123456' ),
-		] );
-	}
+class AdminTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admins')->truncate();
+        DB::table('admins')->insert([
+            'name' => 'TenStudio',
+            'email' => 'admin@drogenidesoftwares.in',
+            'password' => bcrypt('123456'),
+        ], [
+            'name' => 'TenStudio Demo',
+            'email' => 'demo@drogenidesoftwares.in',
+            'password' => bcrypt('123456'),
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,25 +3,25 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::group( [ 'namespace' => 'Consumer\Api' ], function () {
+Route::group(['namespace' => 'Consumer\Api'], function () {
 
-	// auth routes
-	Route::post( 'register', 'RegisterController@register' ); // register
+    // auth routes
+    Route::post('register', 'RegisterController@register'); // register
 
-	Route::group( [ 'middleware' => [ 'auth:api' ] ], function () {
+    Route::group(['middleware' => ['auth:api']], function () {
 
-		// user details update routes
-		Route::post( 'logout', 'LoginController@logout' );
-		Route::post( 'update/name', 'RegisterController@updateName' ); // update name
-		Route::get( '/', 'UserController@index' ); // update user
-		Route::post( '/', 'UserController@update' );
+        // user details update routes
+        Route::post('logout', 'LoginController@logout');
+        Route::post('update/name', 'RegisterController@updateName'); // update name
+        Route::get('/', 'UserController@index'); // update user
+        Route::post('/', 'UserController@update');
 
-		// location routes
-		Route::post( 'location', 'LocationController@update' ); // update location
+        // location routes
+        Route::post('location', 'LocationController@update'); // update location
 
-		// location routes
-		Route::post( 'setting', 'UserCotroller@getSetting' );
+        // location routes
+        Route::post('setting', 'UserCotroller@getSetting');
 
-	} );
+    });
 
-} );
+});
