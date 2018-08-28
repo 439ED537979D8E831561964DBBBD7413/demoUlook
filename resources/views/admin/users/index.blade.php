ef64541f7dce5b9481799efdfc5d82c65b3c16ff
axay <axayhg@gmail.com>
2018-08-28 11:10:51 +0530

    changes

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -13,21 +13,21 @@
                         <thead>
                         <tr role="row">
                             <th class="sorting_asc">ID</th>
-                            <th class="sorting_asc">Status</th>
                             <th class="sorting_asc">First Name</th>
                             <th class="sorting_asc">Last Name</th>
                             <th class="sorting">Mobile Number</th>
-                            <th class="sorting">Certificate Number</th>
+                            <th class="sorting">Payment Mode</th>
+                            <th class="sorting">Rating</th>
                         </tr>
                         </thead>
                         <tfoot>
                         <tr>
                             <th rowspan="1" colspan="1">ID</th>
-                            <th rowspan="1" colspan="1">Status</th>
                             <th rowspan="1" colspan="1">First Name</th>
                             <th rowspan="1" colspan="1">Last Name</th>
                             <th rowspan="1" colspan="1">Mobile Number</th>
-                            <th rowspan="1" colspan="1">Certificate Number</th>
+                            <th rowspan="1" colspan="1">Payment Mode</th>
+                            <th rowspan="1" colspan="1">Rating</th>
                         </tr>
                         </tfoot>
                         <tbody>
@@ -35,11 +35,11 @@
                         @foreach ($users as $user)
                             <tr role="row" class="odd">
                                 <td class="sorting_1">{{ $user->id }}</td>
-                                <td>{{ $user->status }}</td>
                                 <td>{{ $user->first_name }}</td>
                                 <td>{{ $user->last_name }}</td>
                                 <td>{{ $user->mobile }}</td>
-                                <td></td>
+                                <td>{{ $user->payment_mode }}</td>
+                                <td>{{ $user->rating }}</td>
                             </tr>
                         @endforeach
 
