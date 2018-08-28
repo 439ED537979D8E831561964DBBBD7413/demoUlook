c85b1102a966800debaf29d8c63249f2e7fff908
axay <axayhg@gmail.com>
2018-08-28 11:08:01 +0530

    changes

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -2,81 +2,98 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class UserController extends Controller {
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function index() {
-		return view( 'admin.users.index' );
-	}
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function create() {
-		//
-	}
+        //
+        if (!auth()->guard('admin')->check()) {
+            return view('admin.auth.login');
+        }
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @param  \Illuminate\Http\Request $request
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function store( Request $request ) {
-		//
-	}
+        $users = User::orderBy('created_at', 'asc')->paginate(20);
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int $id
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function show( $id ) {
-		//
-	}
+        return view('admin.users.index', compact('users'));
+    }
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int $id
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function edit( $id ) {
-		//
-	}
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  \Illuminate\Http\Request $request
-	 * @param  int $id
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function update( Request $request, $id ) {
-		//
-	}
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int $id
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function destroy( $id ) {
-		//
-	}
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -3,5 +3,58 @@
 @section('title', 'Users');
 
 @section('main')
+    <section class="card">
+        <div class="card-block">
+            <div class="row">
+                <div class="col-sm-12">
+                    <table id="example" class="display table table-striped table-bordered dataTable"
+                           cellspacing="0"
+                           width="100%" role="grid" aria-describedby="example_info" style="width: 100%;">
+                        <thead>
+                        <tr role="row">
+                            <th class="sorting_asc">ID</th>
+                            <th class="sorting_asc">Status</th>
+                            <th class="sorting_asc">First Name</th>
+                            <th class="sorting_asc">Last Name</th>
+                            <th class="sorting">Mobile Number</th>
+                            <th class="sorting">Certificate Number</th>
+                        </tr>
+                        </thead>
+                        <tfoot>
+                        <tr>
+                            <th rowspan="1" colspan="1">ID</th>
+                            <th rowspan="1" colspan="1">Status</th>
+                            <th rowspan="1" colspan="1">First Name</th>
+                            <th rowspan="1" colspan="1">Last Name</th>
+                            <th rowspan="1" colspan="1">Mobile Number</th>
+                            <th rowspan="1" colspan="1">Certificate Number</th>
+                        </tr>
+                        </tfoot>
+                        <tbody>
 
+                        @foreach ($providers as $provider)
+                            <tr role="row" class="odd">
+                                <td class="sorting_1">{{ $provider->id }}</td>
+                                <td>{{ $provider->status }}</td>
+                                <td>{{ $provider->first_name }}</td>
+                                <td>{{ $provider->last_name }}</td>
+                                <td>{{ $provider->mobile }}</td>
+                                <td></td>
+                            </tr>
+                        @endforeach
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
+
+@section('script')
+    <script>
+        $(function () {
+            $('#example').DataTable();
+        });
+    </script>
+@stop
