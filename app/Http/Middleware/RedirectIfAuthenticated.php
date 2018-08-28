fb28f7eeb8c16782d0b92b1580af4344283547eb
axay <axayhg@gmail.com>
2018-08-28 09:21:20 +0530

    changes

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -5,27 +5,29 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Support\Facades\Auth;
 
-class RedirectIfAuthenticated {
-	/**
-	 * Handle an incoming request.
-	 *
-	 * @param  \Illuminate\Http\Request $request
-	 * @param  \Closure $next
-	 * @param  string|null $guard
-	 *
-	 * @return mixed
-	 */
-	public function handle( $request, Closure $next, $guard = null ) {
+class RedirectIfAuthenticated
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @param  string|null $guard
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
 
-		if ( Auth::guard( $guard )->check() ) {
+        if (Auth::guard($guard)->check()) {
 
-			if ( $guard == "admins" ) {
-				return redirect( '/admin/dashboard' );
-			}
+            if ($guard == "admin") {
+                return redirect('/admin/dashboard');
+            }
 
-			return redirect( '/home' );
-		}
+            return redirect('/home');
+        }
 
-		return $next( $request );
-	}
+        return $next($request);
+    }
 }
