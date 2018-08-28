498306c28a68d040893bd8c33e37afbb5034349c
axay <axayhg@gmail.com>
2018-08-28 09:58:41 +0530

    changes

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -16,7 +16,7 @@ class DashboardController extends Controller
     public function index()
     {
 
-        if (auth()->guard('admin')->check()) {
+        if (!auth()->guard('admin')->check()) {
             return view('admin.auth.login');
         }
 
