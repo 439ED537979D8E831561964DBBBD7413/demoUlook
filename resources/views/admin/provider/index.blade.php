5429c0bc0e6badc905d5f82f0d6fe155cdc409a7
axay <axayhg@gmail.com>
2018-08-28 09:46:40 +0530

    changes

--- resources/views/admin/provider/index.blade.php
+++ resources/views/admin/provider/index.blade.php
@@ -7,25 +7,6 @@
         <div class="card-block">
             <div id="example_wrapper" class="dataTables_wrapper container-fluid dt-bootstrap4">
                 <div class="row">
-                    <div class="col-sm-12 col-md-6">
-                        <div class="dataTables_length" id="example_length"><label>Show <select name="example_length"
-                                                                                               aria-controls="example"
-                                                                                               class="form-control form-control-sm">
-                                    <option value="10">10</option>
-                                    <option value="25">25</option>
-                                    <option value="50">50</option>
-                                    <option value="100">100</option>
-                                </select> entries</label></div>
-                    </div>
-                    <div class="col-sm-12 col-md-6">
-                        <div id="example_filter" class="dataTables_filter"><label>Search:<input type="search"
-                                                                                                class="form-control form-control-sm"
-                                                                                                placeholder=""
-                                                                                                aria-controls="example"></label>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
                     <div class="col-sm-12">
                         <table id="example" class="display table table-striped table-bordered dataTable"
                                cellspacing="0"
@@ -270,37 +251,6 @@
                             </tr>
                             </tbody>
                         </table>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-sm-12 col-md-5">
-                        <div class="dataTables_info" id="example_info" role="status" aria-live="polite">Showing 1 to 25
-                            of 57 entries
-                        </div>
-                    </div>
-                    <div class="col-sm-12 col-md-7">
-                        <div class="dataTables_paginate paging_simple_numbers" id="example_paginate">
-                            <ul class="pagination">
-                                <li class="paginate_button page-item previous disabled" id="example_previous"><a
-                                            href="#" aria-controls="example" data-dt-idx="0" tabindex="0"
-                                            class="page-link">Previous</a></li>
-                                <li class="paginate_button page-item active"><a href="#" aria-controls="example"
-                                                                                data-dt-idx="1" tabindex="0"
-                                                                                class="page-link">1</a></li>
-                                <li class="paginate_button page-item "><a href="#" aria-controls="example"
-                                                                          data-dt-idx="2" tabindex="0"
-                                                                          class="page-link">2</a></li>
-                                <li class="paginate_button page-item "><a href="#" aria-controls="example"
-                                                                          data-dt-idx="3" tabindex="0"
-                                                                          class="page-link">3</a></li>
-                                <li class="paginate_button page-item next" id="example_next"><a href="#"
-                                                                                                aria-controls="example"
-                                                                                                data-dt-idx="4"
-                                                                                                tabindex="0"
-                                                                                                class="page-link">Next</a>
-                                </li>
-                            </ul>
-                        </div>
                     </div>
                 </div>
             </div>
